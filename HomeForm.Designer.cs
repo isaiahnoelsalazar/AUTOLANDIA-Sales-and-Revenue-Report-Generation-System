1ce921ed587ea518dece400c9353d5f08a95a483
isaia <isaiahnoelsalazar474@gmail.com>
2025-08-19 19:17:10 +0800

    Updated home form

--- HomeForm.Designer.cs
+++ HomeForm.Designer.cs
@@ -29,41 +29,182 @@
         private void InitializeComponent()
         {
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             this.WeeklySalesChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialDivider1 = new MaterialSkin.Controls.MaterialDivider();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.ViewWeeklySalesButton = new MaterialSkin.Controls.MaterialButton();
+            this.MonthlySalesChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.ViewMonthlySalesButton = new MaterialSkin.Controls.MaterialButton();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             ((System.ComponentModel.ISupportInitialize)(this.WeeklySalesChart)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MonthlySalesChart)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // WeeklySalesChart
             // 
-            this.WeeklySalesChart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
             chartArea1.Name = "ChartArea1";
             this.WeeklySalesChart.ChartAreas.Add(chartArea1);
-            this.WeeklySalesChart.Location = new System.Drawing.Point(17, 17);
-            this.WeeklySalesChart.Margin = new System.Windows.Forms.Padding(8);
+            this.WeeklySalesChart.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.WeeklySalesChart.Location = new System.Drawing.Point(0, 0);
+            this.WeeklySalesChart.Margin = new System.Windows.Forms.Padding(0, 0, 4, 0);
             this.WeeklySalesChart.Name = "WeeklySalesChart";
-            this.WeeklySalesChart.Size = new System.Drawing.Size(606, 326);
+            this.WeeklySalesChart.PaletteCustomColors = new System.Drawing.Color[] {
+        System.Drawing.Color.Red};
+            this.WeeklySalesChart.Size = new System.Drawing.Size(301, 227);
             this.WeeklySalesChart.TabIndex = 0;
             this.WeeklySalesChart.Text = "Weekly Sales";
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Roboto", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.materialLabel1.FontType = MaterialSkin.MaterialSkinManager.fontType.H5;
+            this.materialLabel1.Location = new System.Drawing.Point(15, 15);
+            this.materialLabel1.Margin = new System.Windows.Forms.Padding(6);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(102, 29);
+            this.materialLabel1.TabIndex = 5;
+            this.materialLabel1.Text = "Summary";
+            // 
+            // materialDivider1
+            // 
+            this.materialDivider1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.materialDivider1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialDivider1.Depth = 0;
+            this.materialDivider1.Location = new System.Drawing.Point(15, 56);
+            this.materialDivider1.Margin = new System.Windows.Forms.Padding(6);
+            this.materialDivider1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialDivider1.Name = "materialDivider1";
+            this.materialDivider1.Size = new System.Drawing.Size(610, 2);
+            this.materialDivider1.TabIndex = 4;
+            this.materialDivider1.Text = "materialDivider1";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.MonthlySalesChart, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.WeeklySalesChart, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(15, 72);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(8);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(610, 227);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // ViewWeeklySalesButton
+            // 
+            this.ViewWeeklySalesButton.AutoSize = false;
+            this.ViewWeeklySalesButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ViewWeeklySalesButton.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.ViewWeeklySalesButton.Depth = 0;
+            this.ViewWeeklySalesButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ViewWeeklySalesButton.HighEmphasis = true;
+            this.ViewWeeklySalesButton.Icon = null;
+            this.ViewWeeklySalesButton.Location = new System.Drawing.Point(0, 0);
+            this.ViewWeeklySalesButton.Margin = new System.Windows.Forms.Padding(0, 0, 4, 0);
+            this.ViewWeeklySalesButton.MouseState = MaterialSkin.MouseState.HOVER;
+            this.ViewWeeklySalesButton.Name = "ViewWeeklySalesButton";
+            this.ViewWeeklySalesButton.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.ViewWeeklySalesButton.Size = new System.Drawing.Size(301, 38);
+            this.ViewWeeklySalesButton.TabIndex = 1;
+            this.ViewWeeklySalesButton.Text = "View Weekly Sales Data";
+            this.ViewWeeklySalesButton.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.ViewWeeklySalesButton.UseAccentColor = false;
+            this.ViewWeeklySalesButton.UseVisualStyleBackColor = true;
+            // 
+            // MonthlySalesChart
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.MonthlySalesChart.ChartAreas.Add(chartArea2);
+            this.MonthlySalesChart.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MonthlySalesChart.Location = new System.Drawing.Point(309, 0);
+            this.MonthlySalesChart.Margin = new System.Windows.Forms.Padding(4, 0, 0, 0);
+            this.MonthlySalesChart.Name = "MonthlySalesChart";
+            this.MonthlySalesChart.Size = new System.Drawing.Size(301, 227);
+            this.MonthlySalesChart.TabIndex = 1;
+            this.MonthlySalesChart.Text = "Weekly Sales";
+            // 
+            // ViewMonthlySalesButton
+            // 
+            this.ViewMonthlySalesButton.AutoSize = false;
+            this.ViewMonthlySalesButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ViewMonthlySalesButton.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.ViewMonthlySalesButton.Depth = 0;
+            this.ViewMonthlySalesButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ViewMonthlySalesButton.HighEmphasis = true;
+            this.ViewMonthlySalesButton.Icon = null;
+            this.ViewMonthlySalesButton.Location = new System.Drawing.Point(309, 0);
+            this.ViewMonthlySalesButton.Margin = new System.Windows.Forms.Padding(4, 0, 0, 0);
+            this.ViewMonthlySalesButton.MouseState = MaterialSkin.MouseState.HOVER;
+            this.ViewMonthlySalesButton.Name = "ViewMonthlySalesButton";
+            this.ViewMonthlySalesButton.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.ViewMonthlySalesButton.Size = new System.Drawing.Size(301, 38);
+            this.ViewMonthlySalesButton.TabIndex = 7;
+            this.ViewMonthlySalesButton.Text = "View Monthly Sales Data";
+            this.ViewMonthlySalesButton.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.ViewMonthlySalesButton.UseAccentColor = false;
+            this.ViewMonthlySalesButton.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.ViewWeeklySalesButton, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.ViewMonthlySalesButton, 1, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(15, 310);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(610, 38);
+            this.tableLayoutPanel2.TabIndex = 8;
             // 
             // HomeForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(640, 360);
-            this.Controls.Add(this.WeeklySalesChart);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.materialLabel1);
+            this.Controls.Add(this.materialDivider1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "HomeForm";
             this.Text = "HomeForm";
             ((System.ComponentModel.ISupportInitialize)(this.WeeklySalesChart)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.MonthlySalesChart)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataVisualization.Charting.Chart WeeklySalesChart;
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private MaterialSkin.Controls.MaterialDivider materialDivider1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private MaterialSkin.Controls.MaterialButton ViewWeeklySalesButton;
+        private System.Windows.Forms.DataVisualization.Charting.Chart MonthlySalesChart;
+        private MaterialSkin.Controls.MaterialButton ViewMonthlySalesButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
     }
 }
--- HomeForm.cs
+++ HomeForm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
 using static AUTOLANDIA_Sales_and_Revenue_Report_Generation_System.GlobalValues;
@@ -18,19 +19,36 @@ namespace AUTOLANDIA_Sales_and_Revenue_Report_Generation_System
         {
             WeeklySalesChart.Titles.Clear();
             WeeklySalesChart.Series.Clear();
+            MonthlySalesChart.Titles.Clear();
+            MonthlySalesChart.Series.Clear();
 
             WeeklySalesChart.Titles.Add("Weekly Sales");
             Series DailySales = WeeklySalesChart.Series.Add("Daily Sales");
             DailySales.ChartType = SeriesChartType.Line;
+            DailySales.Color = Color.Red;
+
+            MonthlySalesChart.Titles.Add("Monthly Sales");
+            Series MonthlySales = MonthlySalesChart.Series.Add("Monthly Sales");
+            MonthlySales.ChartType = SeriesChartType.Line;
+            MonthlySales.Color = Color.Red;
+            MonthlySalesChart.ChartAreas[0].AxisX.Interval = 1;
 
             DateTime Today = DateTime.Now;
             DateTime[] Days = new DateTime[7];
+            DateTime[] Months = new DateTime[12];
 
             DateTime StartOfWeek = Today.AddDays(-(int)Today.DayOfWeek);
+            DateTime StartOfYear = DateTime.Parse(Today.AddMonths(-Today.Month + 1).ToString("Y"));
+
             for (int a = 0; a < 7; a++)
             {
                 Days[a] = StartOfWeek.AddDays(a);
                 DailySales.Points.AddXY(Days[a].ToString("D"), 0);
+            }
+            for (int a = 0; a < 12; a++)
+            {
+                Months[a] = StartOfYear.AddMonths(a);
+                MonthlySales.Points.AddXY(Months[a].ToString("MMMM"), 0);
             }
 
             for (int a = 0; a < Days.Length; a++)
@@ -39,13 +57,27 @@ namespace AUTOLANDIA_Sales_and_Revenue_Report_Generation_System
 
                 foreach (OrderItem Order in OrderList)
                 {
-                    if (Days[a].ToString("d").Equals(DateTime.Parse(Order.DateCreated).ToString("d")))
+                    if (DateTime.Parse(Order.DateCreated).ToString("d").Equals(Days[a].ToString("d")))
                     {
                         TotalSales += Convert.ToDouble(Order.OrderBalance);
                     }
                 }
 
                 DailySales.Points[a].SetValueY(TotalSales);
+            }
+            for (int a = 0; a < Months.Length; a++)
+            {
+                double TotalSales = 0;
+
+                foreach (OrderItem Order in OrderList)
+                {
+                    if (DateTime.Parse(Order.DateCreated).ToString("Y").Equals(Months[a].ToString("Y")))
+                    {
+                        TotalSales += Convert.ToDouble(Order.OrderBalance);
+                    }
+                }
+
+                MonthlySales.Points[a].SetValueY(TotalSales);
             }
         }
     }
